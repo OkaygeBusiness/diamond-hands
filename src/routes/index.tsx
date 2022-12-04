713208b1ebbac8834ebcedf078779faca7ae839a
iantelli <2ndex@duck.com>
2022-12-04 15:55:36 -0800

    :label: Updated onClick button prop type

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { component$ } from "@builder.io/qwik"
+import { $, component$ } from "@builder.io/qwik"
 import type { DocumentHead } from "@builder.io/qwik-city"
 import HeaderText from "../components/atoms/headerText/headerText"
 import SubHeaderText from "../components/atoms/subHeaderText/subHeaderText"
