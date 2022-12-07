f6616aa2d6716d2f0122ab34e3a82fd639fc572c
iantelli <2ndex@duck.com>
2022-12-07 00:34:44 -0800

    :poop: Calculation util

--- src/utils/currency.ts
+++ src/utils/currency.ts
@@ -3,3 +3,11 @@
 export const formatCurrency = (value: number) => {
   return console.log("Method not implemented.")
 }
+
+export const calculateBrokerFee = (value: number) => {
+  return parseInt((value * 0.02).toFixed(2)) // 2% broker fee
+}
+
+export const calculateTotal = (value: number, amount: number) => {
+  return parseInt((value * amount + calculateBrokerFee(value * amount)).toFixed(2))
+}
