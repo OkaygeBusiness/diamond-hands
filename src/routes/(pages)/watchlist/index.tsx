4b71ffa497d620817dbff1b380fd552de5c58fe3
iantelli <2ndex@duck.com>
2022-12-07 01:29:04 -0800

    :art: Watchlist & quiz page

--- src/routes/(pages)/watchlist/index.tsx
+++ src/routes/(pages)/watchlist/index.tsx
@@ -1,12 +1,25 @@
-import { component$ } from "@builder.io/qwik"
+import { $, component$, useStore } from "@builder.io/qwik"
 import Header from "../../../components/molecules/header/header"
 import Button from "../../../components/atoms/button/button"
+import WatchStock from "../../../components/molecules/watchStock/watchStock"
+import * as UserService from "../../../db/api/userService"
+import { Stock, WatchList } from "../../../types"
 
 export default component$(() => {
+  const user = UserService.getUser(1)
+  const watchlist = useStore<WatchList>(user?.watchlist!)
+
+  const removeStock = $(() => {
+    watchlist?.pop()
+  })
+
   return (
     <>
       <Header text="Watchlist" hasButton={true} />
       <Button text="Add" color="bg-lightGreen" />
+      {watchlist?.map((stock, i) => {
+        return <WatchStock key={i} stock={stock} change="0.00" isPositive={true} onClick={removeStock} />
+      })}
     </>
   )
 })
