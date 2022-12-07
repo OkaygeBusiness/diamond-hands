b2a1ff4f99378ef2624d14c5a0698f6aacb582f0
iantelli <2ndex@duck.com>
2022-12-06 19:45:21 -0800

    :lipstick: Header component and link button

---
+++ src/components/molecules/header/header.tsx
@@ -0,0 +1,32 @@
+import { $, component$, QwikMouseEvent } from "@builder.io/qwik"
+import { twMerge } from "tailwind-merge"
+import LinkButton from "../linkButton/linkButton"
+import HeaderText from "../../atoms/headerText/headerText"
+
+export interface headerProps {
+  text: string
+  backgroundColor?: string
+  hasButton?: boolean
+  link?: string
+  class?: string
+}
+
+export default component$((props: headerProps) => {
+  return (
+    <>
+      <div class="fixed inset-x-0 top-0">
+        <div class={twMerge(props.class, "flex py-12 justify-between bg-darkBlue")}>
+          <div>
+            {props.hasButton && (
+              <LinkButton image="/assets/icons/backArrow.png" class="ml-6 -mr-12" color="bg-gray-300 text-black" link={props.link} />
+            )}
+          </div>
+          <div>
+            <HeaderText text={props.text} size="text-3xl" />
+          </div>
+          <div>{/* for scuffed spacing */}</div>
+        </div>
+      </div>
+    </>
+  )
+})
