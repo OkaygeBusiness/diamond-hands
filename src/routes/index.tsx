c9cda7a98d5fe3e5667377adec65b74e3829ae99
iantelli <2ndex@duck.com>
2022-12-07 13:16:05 -0800

    :sparkles: Homepage now shows real user data

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,22 +2,23 @@ import { $, component$ } from "@builder.io/qwik"
 import type { DocumentHead } from "@builder.io/qwik-city"
 import HeaderText from "../components/atoms/headerText/headerText"
 import SubHeaderText from "../components/atoms/subHeaderText/subHeaderText"
-import Button, { buttonSize } from "../components/atoms/button/button"
 import MiniCard from "../components/molecules/miniCard/miniCard"
 import BigCard from "../components/molecules/bigCard/bigCard"
+import * as UserService from "../db/api/userService"
+import { calculateWalletValue } from "../utils"
 
 export default component$(() => {
-  const name = "John Doe"
+  const user = UserService.getUser(1)
   return (
     <>
       <header class="flex">
         <img class="h-12 w-12" src="/assets/logo-black.png" alt="logo" />
         <div>
-          <HeaderText text={name} color="text-black" />
-          <SubHeaderText class="text-xl font-bold text-gray-400" text={`Welcome back ${name.split(" ")[0]}!`} />
+          <HeaderText text={user!.name} color="text-black" />
+          <SubHeaderText class="text-xl font-bold text-gray-400" text={`Welcome back ${user!.name.split(" ")[0]}!`} />
         </div>
       </header>
-      <BigCard headerText="Value" text="14,727$" buttonText="Stocks" gain="7.27%" isPositive={true} />
+      <BigCard headerText="Value" text={`${calculateWalletValue(user!.wallet!).toString()}$`} buttonText="Stocks" gain="7.27%" isPositive={true} />
       <MiniCard text="Watchlist" subText="View your favourited stocks" buttonText="View" image="/assets/icons/star.png" link="/watchlist" />
       <MiniCard text="Trade History" subText="View your recent exchanges" buttonText="View" image={"/assets/icons/history.png"} link="/" />
       {/* example of using button component props to change button size or add a logo*/}
