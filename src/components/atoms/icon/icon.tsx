9ceef17fb14e220924d6324cedcb8f6c9fabf1f0
iantelli <2ndex@duck.com>
2022-12-06 17:49:00 -0800

    :art: MiniCard adjustments

--- src/components/atoms/icon/icon.tsx
+++ src/components/atoms/icon/icon.tsx
@@ -8,5 +8,5 @@ export interface iconProps {
 }
 
 export default component$((props: iconProps) => {
-  return <img class={twMerge(props.class, props.height ?? "h-6")} src={props.image} alt="icon" />
+  return <img class={twMerge(props.class, props.height ?? "h-6")} src={props.image} />
 })
