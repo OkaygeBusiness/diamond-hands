4ac4d2f766dde9a2c25a67a325fe18107ee94791
iantelli <2ndex@duck.com>
2022-12-07 01:29:17 -0800

    :lipstick: Watchlist stock component

--- src/components/atoms/icon/icon.tsx
+++ src/components/atoms/icon/icon.tsx
@@ -1,12 +1,24 @@
-import { component$ } from "@builder.io/qwik"
+import { $, component$, QwikMouseEvent } from "@builder.io/qwik"
 import { twMerge } from "tailwind-merge"
 
 export interface iconProps {
   image?: string
   height?: string
+  onClick?: (event: QwikMouseEvent) => void
   class?: string
 }
 
 export default component$((props: iconProps) => {
-  return <img class={twMerge(props.class, props.height ?? "h-6")} src={props.image} />
+  return (
+    <img
+      class={twMerge(props.class, props.height ?? "h-6")}
+      src={props.image}
+      onClick$={
+        props.onClick ??
+        $(() => {
+          console.log("Icon clicked")
+        })
+      }
+    />
+  )
 })
