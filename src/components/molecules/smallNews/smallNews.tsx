0d8e34400746ce9338ed2819f01fed9abab77779
ian chao <2ndex@duck.com>
2022-12-07 19:17:16 -0800

    Update smallNews.tsx

--- src/components/molecules/smallNews/smallNews.tsx
+++ src/components/molecules/smallNews/smallNews.tsx
@@ -20,11 +20,7 @@ export default component$((props: smallNewsProps) => {
         <div class="flex flex-row bg-white border rounded-lg shadow-md hover:bg-gray-100 mb-2">
           <Icon class="object-cover w-0 lg:w-80 rounded-l-lg" image={props.image} height="h-auto" />
           <div class="flex flex-col justify-between p-4 leading-normal">
-<<<<<<< Updated upstream
             <HeaderText class="mb-2 text-sm font-bold tracking-tight text-gray-900 " text={props.headerText} size="text-xl" color="text-black" />
-=======
-            <HeaderText class="mb-2 font-bold tracking-tight text-gray-900 " text={props.headerText} size="text-md" color="text-black" />
->>>>>>> Stashed changes
             <SubHeaderText class="mb-3 font-normal text-gray-700 " text={props.text} size="text-xs" />
           </div>
         </div>
